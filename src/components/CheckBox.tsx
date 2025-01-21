c5983c0b876e1dc50b58744a86ccc3c281f193a2
aaguinagalde <aaguinagalde@transmandu.com>
2025-01-21 15:45:39 -0400

    Modificacion al componente check para mostrar multiples check y guardarlos en un array de string 2

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import { useState, useEffect } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
   FormControl,
@@ -12,30 +11,56 @@ import { FieldValues, Path, UseFormReturn } from "react-hook-form";
 
 type CheckProp<T extends FieldValues> = {
   form: UseFormReturn<T>;
+  title: string;
   name: Path<T>;
-  label: string;
-  description: string;
+  label?: string;
+  options: string[];
 };
 
 export default function CheckBox<T extends FieldValues>({
   form,
   name,
   label,
-  description,
+  options,
 }: CheckProp<T>) {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(
+    (form.getValues(name) as string[]) || []
+  );
+
+  const handleCheckboxChange = (option: string) => {
+    setSelectedOptions((prevOptions) =>
+      prevOptions.includes(option)
+        ? prevOptions.filter((opt) => opt !== option)
+        : [...prevOptions, option]
+    );
+  };
+
+  useEffect(() => {
+    form.setValue(name, selectedOptions as any); // Aseguramos que el tipo sea correcto
+  }, [selectedOptions, form, name]);
+
   return (
     <FormField
       control={form.control}
       name={name}
       render={({ field }) => (
-        <FormItem className="flex items-center space-x-2 rounded-md border p-4">
-          <FormControl>
-            <Checkbox checked={field.value} onCheckedChange={field.onChange} />
-          </FormControl>
+        <FormItem className="flex flex-col space-y-2 rounded-md border p-4 ">
           <FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
             {label}
           </FormLabel>
-          <FormMessage>{description}</FormMessage>
+          {options.map((option) => (
+            <FormControl key={option}>
+              <div className="flex items-center space-x-2">
+                <Checkbox
+                  checked={selectedOptions.includes(option)}
+                  onCheckedChange={() => handleCheckboxChange(option)}
+                />
+                <FormLabel className=" text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  {option}
+                </FormLabel>
+              </div>
+            </FormControl>
+          ))}
         </FormItem>
       )}
     />
